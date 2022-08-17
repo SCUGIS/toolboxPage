953a472c94cf06f548b1a51227ead021b72bb1c1
rsghost <im@rsghost.org>
2022-08-17 15:06:34 +0800

    fix: 🐛 render

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 const config = {
  jsonapi: {
    base: 'https://gis.scu.edu.tw/jsonapi?id=1zTXQUPXSY1OuL5rAMX_5CKWCCWTeQtXT9LTN7R_aIT4',
-   services: '&sheet=service',
-   codes: '&sheet=code',
+   services: '&sheet=services',
+   repos: '&sheet=repos',
  },
  services: [],
- codes: [],
+ repos: [],
 }
 
 
@@ -15,20 +15,30 @@ const init = async () => {
     type: 'GET',
   })
 
-  const codes = await $.ajax({
-    url: `${config.jsonapi.base}${config.jsonapi.codes}`,
+  const repos = await $.ajax({
+    url: `${config.jsonapi.base}${config.jsonapi.repos}`,
     type: 'GET',
   })
 
 
-  config.codes = codes.rows
+  config.repos = repos.rows
   config.services = services.rows
 
   await render(0, config.services)
 }
 
+$('#reposRender').click(async () => {
+  await render(1, config.repos)
+})
+
+$('#servicesRender').click(async () => {
+  await render(0, config.services)
+})
+
 const render = async (type, services) => {
   const base = $('.base')
+  $('.page').empty()
+
   for (const service of services) {
     const dom = base.clone()
 
@@ -44,6 +54,14 @@ const render = async (type, services) => {
         $('.page').append(dom)
         break
       case 1:
+        dom.removeClass('base')
+        dom.find('.base-name').text(service.name)
+        dom.find('.base-description').text(service.description)
+        // dom.find('.base-image').attr('src', service.image)
+        dom.find('.base-path').attr('href', service.path)
+        // dom.find('.base-author').text(service.author)
+
+        $('.page').append(dom)
         break
       default:
     }
